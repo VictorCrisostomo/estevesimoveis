3b8725e1911cfa25b31c6a09213c0f20b7339d6b
VictorCrisostomo <victorcrisostomoliveira@gmail.com>
2023-11-28 16:46:49 -0300

    edit main page

--- src/components/ui/mainPage/header/Hero.tsx
+++ src/components/ui/mainPage/header/Hero.tsx
@@ -1,6 +1,18 @@
+'use client';
+
 import Image from 'next/image';
 
-import imgbg from "@/assets/IMG_ok 0010.jpg"
+import img1 from "@/assets/imgs/DJI_ok 0056.jpg"
+import img2 from "@/assets/imgs/DJI_ok 0180.jpg"
+import img3 from "@/assets/imgs/IMG_ok 0010.jpg"
+import img4 from "@/assets/imgs/IMG_ok 0037.jpg"
+import img5 from "@/assets/imgs/IMG_ok 0049.jpg"
+import img6 from "@/assets/imgs/IMG_ok 0057.jpg"
+import img7 from "@/assets/imgs/IMG_ok 9966.jpg"
+
+
+
+import { Carousel } from 'flowbite-react';
 
 export function Hero() {
   return (
@@ -10,14 +22,40 @@ export function Hero() {
         <video className="min-h-screen w-[100%] h-[100%] object-cover hidden md:block absolute opacity-70" loop autoPlay muted>
             <source src={"video/video.mp4"} />
         </video>
-        <Image src={imgbg} alt="" className='object-cover "min-h-screen w-[100%] h-[100%] absolute md:hidden opacity-70' />
         
+      <Carousel className='md:hidden cursor-default'>
+        <div className="relative flex items-center justify-center object-cover">
+          <Image src={img1} alt="" className='object-cover min-h-screen w-[100%] h-[100%] opacity-70' />
+        </div>
+        <div className="relative flex items-center justify-center object-cover">
+          <Image src={img2} alt="" className='object-cover min-h-screen w-[100%] h-[100%] opacity-70' />
+        </div>
+        <div className="relative flex items-center justify-center object-cover">
+          <Image src={img3} alt="" className='object-cover min-h-screen w-[100%] h-[100%] opacity-70' />
+        </div>
+        <div className="relative flex items-center justify-center object-cover">
+          <Image src={img4} alt="" className='object-cover min-h-screen w-[100%] h-[100%] opacity-70' />
+        </div>
+        <div className="relative flex items-center justify-center object-cover">
+          <Image src={img5} alt="" className='object-cover min-h-screen w-[100%] h-[100%] opacity-70' />
+        </div>
+        <div className="relative flex items-center justify-center object-cover">
+          <Image src={img6} alt="" className='object-cover min-h-screen w-[100%] h-[100%] opacity-70' />
+        </div>
+        <div className="relative flex items-center justify-center object-cover">
+          <Image src={img7} alt="" className='object-cover min-h-screen w-[100%] h-[100%] opacity-70' />
+        </div>
+
+
+      </Carousel>
+
+
         {/* Texts */}
-        <div className="mx-8 md:mx-48">
-          <h1 className="text-4xl md:text-6xl font-bold relative text-white text-center drop-shadow-lg shadow-black">Esteves Imóveis</h1>
+        <div className="mx-8 md:mx-48 absolute">
+          <h1 className="text-4xl md:text-6xl font-bold relative text-white text-center drop-shadow-md shadow-black">Esteves Imóveis</h1>
           <br />
-          <p className="relative text-white text-center drop-shadow-lg shadow-black">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, saepe. Exercitationem molestiae magni est, consectetur expedita inventore sit dolore accusantium laboriosam optio quae cupiditate minima quidem perferendis non, illum praesentium.
+          <p className="relative text-white text-center drop-shadow-md shadow-black">
+          Encontre o seu lar e os melhores investimentos imobiliários em Canavieiras-Ba. Estamos há mais de 11 anos realizando sonhos de morar bem!
           </p>
         </div>
       </section>
@@ -25,7 +63,7 @@ export function Hero() {
 
 
 
-
+ 
 
   )
 }
