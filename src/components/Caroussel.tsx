8c488d6d1efed4bc322d703bb595aaa37a80f095
VictorCrisostomo <victorcrisostomoliveira@gmail.com>
2024-01-24 17:13:24 -0300

    add novos banners

--- src/components/Caroussel.tsx
+++ src/components/Caroussel.tsx
@@ -5,10 +5,10 @@ import { Carousel } from 'flowbite-react';
 export function Caroussel() {
   return (
     <Carousel>
-      <div className="relative flex items-center justify-center min-w-screen h-44 md:h-80 bg-[url(../assets/banner.jpg)] bg-cover bg-center"/>
-      <div className="relative flex items-center justify-center min-w-screen h-44 md:h-80 bg-[url(../assets/banner.jpg)] bg-cover bg-center"/>
-      <div className="relative flex items-center justify-center min-w-screen h-44 md:h-80 bg-[url(../assets/banner.jpg)] bg-cover bg-center"/>
-      <div className="relative flex items-center justify-center min-w-screen h-44 md:h-80 bg-[url(../assets/banner.jpg)] bg-cover bg-center"/>
+      <div className="relative flex items-center justify-center min-w-screen h-44 md:h-80 bg-[url(../assets/banner1.jpg)] bg-cover bg-center"/>
+      <div className="relative flex items-center justify-center min-w-screen h-44 md:h-80 bg-[url(../assets/banner2.jpg)] bg-cover bg-center"/>
+      <div className="relative flex items-center justify-center min-w-screen h-44 md:h-80 bg-[url(../assets/banner3.jpg)] bg-cover bg-center"/>
+      <div className="relative flex items-center justify-center min-w-screen h-44 md:h-80 bg-[url(../assets/banner4.jpg)] bg-cover bg-center"/>
     </Carousel>
   )
 }
